ddec72a57bb419d909d80e3094a396f8e9a84c6c
Karel Peeters <karel.peeters.leuven@gmail.com>
2024-07-08 21:37:58 +0200

    Advertise semantic token highlighting, not yet implemented

--- rust/lsp_server/src/server/lifecycle.rs
+++ rust/lsp_server/src/server/lifecycle.rs
@@ -1,5 +1,10 @@
 use tower_lsp::jsonrpc;
-use tower_lsp::lsp_types::{CompletionOptions, DefinitionOptions, DocumentHighlightOptions, InitializeParams, InitializeResult, InitializedParams, MessageType, OneOf, ServerCapabilities, ServerInfo, TextDocumentSyncCapability, TextDocumentSyncKind, TextDocumentSyncOptions, WorkDoneProgressOptions};
+use tower_lsp::lsp_types::{
+    CompletionOptions, DefinitionOptions, DocumentHighlightOptions, InitializeParams, InitializeResult,
+    InitializedParams, MessageType, OneOf, SemanticTokenType, SemanticTokensFullOptions, SemanticTokensLegend,
+    SemanticTokensOptions, SemanticTokensServerCapabilities, ServerCapabilities, ServerInfo,
+    TextDocumentSyncCapability, TextDocumentSyncKind, TextDocumentSyncOptions, WorkDoneProgressOptions,
+};
 
 use crate::server::core::ServerCore;
 
@@ -15,7 +20,7 @@ impl ServerCore {
         self.log_info(format!("client params: {:#?}", params)).await;
 
         Ok(InitializeResult {
-            capabilities: CAPABILITIES,
+            capabilities: capabilities(),
             server_info: Some(server_info),
         })
     }
@@ -34,55 +39,75 @@ impl ServerCore {
     }
 }
 
-const CAPABILITIES: ServerCapabilities = ServerCapabilities {
-    position_encoding: None,
-    text_document_sync: Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
-        open_close: Some(true),
-        // TODO support incremental file changes
-        change: Some(TextDocumentSyncKind::FULL),
-        will_save: Some(false),
-        will_save_wait_until: Some(false),
-        save: None,
-    })),
-    selection_range_provider: None,
-    hover_provider: None,
-    completion_provider: Some(CompletionOptions {
-        resolve_provider: Some(true),
-        trigger_characters: None,
-        all_commit_characters: None,
-        work_done_progress_options: WorkDoneProgressOptions { work_done_progress: None },
-        completion_item: None,
-    }),
-    signature_help_provider: None,
-    definition_provider: Some(OneOf::Right(DefinitionOptions {
-        work_done_progress_options: WorkDoneProgressOptions { work_done_progress: None },
-    })),
-    type_definition_provider: None,
-    implementation_provider: None,
-    references_provider: None,
-    document_highlight_provider: Some(OneOf::Right(DocumentHighlightOptions {
-        work_done_progress_options: WorkDoneProgressOptions { work_done_progress: None },
-    })),
-    document_symbol_provider: None,
-    workspace_symbol_provider: None,
-    code_action_provider: None,
-    code_lens_provider: None,
-    document_formatting_provider: None,
-    document_range_formatting_provider: None,
-    document_on_type_formatting_provider: None,
-    rename_provider: None,
-    document_link_provider: None,
-    color_provider: None,
-    folding_range_provider: None,
-    declaration_provider: None,
-    execute_command_provider: None,
-    workspace: None,
-    call_hierarchy_provider: None,
-    semantic_tokens_provider: None,
-    moniker_provider: None,
-    linked_editing_range_provider: None,
-    inline_value_provider: None,
-    inlay_hint_provider: None,
-    diagnostic_provider: None,
-    experimental: None,
+const NO_PROGRESS: WorkDoneProgressOptions = WorkDoneProgressOptions {
+    work_done_progress: None,
 };
+
+fn capabilities() -> ServerCapabilities {
+    ServerCapabilities {
+        position_encoding: None,
+        text_document_sync: Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
+            open_close: Some(true),
+            // TODO support incremental file changes
+            change: Some(TextDocumentSyncKind::FULL),
+            will_save: Some(false),
+            will_save_wait_until: Some(false),
+            save: None,
+        })),
+        selection_range_provider: None,
+        hover_provider: None,
+        completion_provider: Some(CompletionOptions {
+            resolve_provider: Some(true),
+            trigger_characters: None,
+            all_commit_characters: None,
+            work_done_progress_options: NO_PROGRESS,
+            completion_item: None,
+        }),
+        signature_help_provider: None,
+        definition_provider: Some(OneOf::Right(DefinitionOptions {
+            work_done_progress_options: NO_PROGRESS,
+        })),
+        type_definition_provider: None,
+        implementation_provider: None,
+        references_provider: None,
+        document_highlight_provider: Some(OneOf::Right(DocumentHighlightOptions {
+            work_done_progress_options: NO_PROGRESS,
+        })),
+        document_symbol_provider: None,
+        workspace_symbol_provider: None,
+        code_action_provider: None,
+        code_lens_provider: None,
+        document_formatting_provider: None,
+        document_range_formatting_provider: None,
+        document_on_type_formatting_provider: None,
+        rename_provider: None,
+        document_link_provider: None,
+        color_provider: None,
+        folding_range_provider: None,
+        declaration_provider: None,
+        execute_command_provider: None,
+        workspace: None,
+        call_hierarchy_provider: None,
+        // TODO registration?
+        semantic_tokens_provider: Some(SemanticTokensServerCapabilities::SemanticTokensOptions(
+            SemanticTokensOptions {
+                work_done_progress_options: NO_PROGRESS,
+                legend: SemanticTokensLegend {
+                    token_types: SEMANTIC_TOKEN_LEGEND.to_vec(),
+                    token_modifiers: vec![],
+                },
+                range: Some(false),
+                full: Some(SemanticTokensFullOptions::Delta { delta: Some(false) }),
+            },
+        )),
+        moniker_provider: None,
+        linked_editing_range_provider: None,
+        inline_value_provider: None,
+        inlay_hint_provider: None,
+        diagnostic_provider: None,
+        experimental: None,
+    }
+}
+
+// TODO make this faster through a real enum
+const SEMANTIC_TOKEN_LEGEND: &[SemanticTokenType] = &[SemanticTokenType::KEYWORD, SemanticTokenType::STRING];
