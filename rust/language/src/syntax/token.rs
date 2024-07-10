f24f9e1c8cac4eb3a5f3603ed904a9d59658e592
Karel Peeters <karel.peeters.leuven@gmail.com>
2024-07-10 21:17:47 +0200

    Simplify and fix bugs in token matching

--- rust/language/src/syntax/token.rs
+++ rust/language/src/syntax/token.rs
@@ -49,7 +49,7 @@ pub fn tokenize(file: FileId, source: &str) -> Result<Vec<Token<&str>>, InvalidT
         let left_context = &left[..min(left.len(), ERROR_CONTEXT_LENGTH)];
         let matches = regex_set.matches(left);
 
-        let match_index = match pick_match(matches, &regex_vec, left) {
+        let m = match pick_match(matches, &regex_vec, left) {
             None => {
                 return Err(InvalidToken {
                     pos,
@@ -59,18 +59,15 @@ pub fn tokenize(file: FileId, source: &str) -> Result<Vec<Token<&str>>, InvalidT
             Some(match_index) => match_index,
         };
 
-        let match_range = regex_vec[match_index].find(left).unwrap().range();
-        assert!(match_range.start == 0);
-
-        let match_str = &left[..match_range.end];
-        left = &left[match_range.end..];
+        let match_str = &left[..m.len];
+        left = &left[m.len..];
 
         let start = pos;
         pos = pos.step_over(match_str);
         let span = Span::new(start, pos);
 
         tokens.push(Token {
-            ty: TOKEN_PATTERNS[match_index].0,
+            ty: TOKEN_PATTERNS[m.index].0,
             string: match_str,
             span,
         })
@@ -79,85 +76,60 @@ pub fn tokenize(file: FileId, source: &str) -> Result<Vec<Token<&str>>, InvalidT
     Ok(tokens)
 }
 
-// Picking algorithm:
-// * Fail if an any of:
-//   * multiple matches with at least one TokenPriority::Unique
-//   * regex match and at least one other regex/literal match with same priority
-//   * multiple literal matches with same priority and same length
-// Tiebreak:
-//   * if different priorities, the highest priority wins.
-//   * if same priority, the longest one wins.
-fn pick_match(matches: SetMatches, regex_vec: &[Regex], left: &str) -> Option<usize> {
-    // TODO does this implementation really need to be this ugly?
-    let mut match_count: u32 = 0;
+struct PickedMatch {
+    index: usize,
+    len: usize,
+    prio: TokenPriority,
+}
 
-    let mut unique = None;
-
-    let mut longest_high: Option<(usize, usize)> = None;
-    let mut longest_medium = None;
-    let mut longest_low = None;
-
-    let mut regex_high = None;
-    let mut regex_medium = None;
-    let mut regex_low = None;
-
-    fn try_longest_literal(curr_index: usize, curr_len: usize, slot: &mut Option<(usize, usize)>) {
-        let better = match *slot {
-            None => true,
-            Some((_, other_len)) => {
-                assert_ne!(
-                    curr_len, other_len,
-                    "multiple matching literals with same length and priority should be impossible"
-                );
-                curr_len > other_len
-            }
-        };
-        if better {
-            *slot = Some((curr_index, curr_len));
-        }
-    }
-
-    fn try_regex(curr_index: usize, slot: &mut Option<usize>) {
-        assert!(slot.is_none());
-        *slot = Some(curr_index);
-    }
+fn pick_match(matches: SetMatches, regex_vec: &[Regex], left: &str) -> Option<PickedMatch> {
+    let mut unique = false;
+    let mut result: Option<PickedMatch> = None;
+    let mut match_count = 0;
 
     for index in matches.iter() {
-        let (_, pattern, kind, prio) = TOKEN_PATTERNS[index];
         match_count += 1;
-        match kind {
-            PK::Regex => match prio {
-                TP::High => try_regex(index, &mut regex_high),
-                TP::Medium => try_regex(index, &mut regex_medium),
-                TP::Low => try_regex(index, &mut regex_low),
-                TP::Unique => unique = Some(index),
-            },
-            PK::Literal => match prio {
-                TP::High => try_longest_literal(index, pattern.len(), &mut longest_high),
-                TP::Medium => try_longest_literal(index, pattern.len(), &mut longest_medium),
-                TP::Low => try_longest_literal(index, pattern.len(), &mut longest_low),
-                TP::Unique => unique = Some(index),
-            },
+
+        let range = regex_vec[index].find(left).unwrap().range();
+        assert_eq!(range.start, 0);
+        let len = range.end;
+
+        let (_, _, _, prio) = TOKEN_PATTERNS[index];
+        match prio {
+            TP::Unique => unique = true,
+            TP::Normal | TP::Low => {}
+        }
+
+        let better = match result {
+            Some(PickedMatch {
+                index: prev_index,
+                len: prev_len,
+                prio: prev_prio,
+            }) => {
+                let key = (len, prio);
+                let prev_key = (prev_len, prev_prio);
+                assert_ne!(
+                    key,
+                    prev_key,
+                    "tokens {:?} and {:?} both with priority {:?} match same prefix {:?}",
+                    TOKEN_PATTERNS[prev_index].0,
+                    TOKEN_PATTERNS[index].0,
+                    prio,
+                    &left[..len],
+                );
+                key > prev_key
+            }
+            None => true,
+        };
+        if better {
+            result = Some(PickedMatch { index, len, prio });
         }
     }
 
-    if let Some(unique) = unique {
+    if unique {
         assert!(match_count == 1);
-        return Some(unique);
     }
-
-    fn pair(regex: Option<usize>, literal: Option<(usize, usize)>) -> Option<usize> {
-        match (regex, literal) {
-            (None, None) => None,
-            (Some(index), None) | (None, Some((index, _))) => Some(index),
-            (Some(_), Some(_)) => panic!("Conflict between regex and literal"),
-        }
-    }
-
-    // this evaluated all pairs early for extra error checking
-    pair(regex_high, longest_high)
-        .or(pair(regex_medium, longest_medium))
-        .or(pair(regex_low, longest_low))
+    result
 }
 
 macro_rules! declare_tokens {
@@ -199,12 +171,12 @@ pub enum TokenCategory {
     Symbol,
 }
 
-#[derive(Debug, Copy, Clone)]
+/// Priority is only used to tiebreak equal-length matches.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum TokenPriority {
-    High,
-    Medium,
-    Low,
-    Unique,
+    Unique = 2,
+    Normal = 1,
+    Low = 0,
 }
 
 use PatternKind as PK;
@@ -214,11 +186,11 @@ use TokenPriority as TP;
 declare_tokens! {
     // ignored
     WhiteSpace(r"\s+", PK::Regex, TC::WhiteSpace, TP::Unique),
-    LineComment(r"//[^\n\r]*", PK::Regex, TC::Comment, TP::High),
-    BlockComment(r"/\*([^\*]*\*+[^\*/])*([^\*]*\*+|[^\*])*\*/", PK::Regex, TC::Comment, TP::High),
+    LineComment(r"//[^\n\r]*", PK::Regex, TC::Comment, TP::Normal),
+    BlockComment(r"/\*([^\*]*\*+[^\*/])*([^\*]*\*+|[^\*])*\*/", PK::Regex, TC::Comment, TP::Normal),
 
     // patterns
-    Identifier(r"(_[a-zA-Z_0-9]+)|([a-zA-Z][a-zA-Z_0-9]*)", PK::Regex, TC::Identifier, TP::Medium),
+    Identifier(r"(_[a-zA-Z_0-9]+)|([a-zA-Z][a-zA-Z_0-9]*)", PK::Regex, TC::Identifier, TP::Low),
     IntLiteralDecimal(r"[0-9]+", PK::Regex, TC::IntegerLiteral, TP::Unique),
     IntPatternHexadecimal(r"0x[0-9a-fA-F_?]+", PK::Regex, TC::IntegerLiteral, TP::Unique),
     IntPatternBinary(r"0b[0-9a-fA-F_?]+", PK::Regex, TC::IntegerLiteral, TP::Unique),
@@ -227,87 +199,87 @@ declare_tokens! {
     StringLiteral(r#""[^"]*""#, PK::Regex, TC::StringLiteral, TP::Unique),
 
     // keywords
-    Use("use", PK::Literal, TC::Keyword, TP::High),
-    As("as", PK::Literal, TC::Keyword, TP::High),
-    Type("type", PK::Literal, TC::Keyword, TP::High),
-    Struct("struct", PK::Literal, TC::Keyword, TP::High),
-    Enum("enum", PK::Literal, TC::Keyword, TP::High),
-    Ports("ports", PK::Literal, TC::Keyword, TP::High),
-    Module("module", PK::Literal, TC::Keyword, TP::High),
-    Function("function", PK::Literal, TC::Keyword, TP::High),
-    Combinatorial("combinatorial", PK::Literal, TC::Keyword, TP::High),
-    Clocked("clocked", PK::Literal, TC::Keyword, TP::High),
-    Const("const", PK::Literal, TC::Keyword, TP::High),
-    Val("val", PK::Literal, TC::Keyword, TP::High),
-    Var("var", PK::Literal, TC::Keyword, TP::High),
-    Input("input", PK::Literal, TC::Keyword, TP::High),
-    Output("output", PK::Literal, TC::Keyword, TP::High),
-    Async("async", PK::Literal, TC::Keyword, TP::High),
-    Sync("sync", PK::Literal, TC::Keyword, TP::High),
-    Return("return", PK::Literal, TC::Keyword, TP::High),
-    Break("break", PK::Literal, TC::Keyword, TP::High),
-    Continue("continue", PK::Literal, TC::Keyword, TP::High),
-    True("true", PK::Literal, TC::Keyword, TP::High),
-    False("false", PK::Literal, TC::Keyword, TP::High),
-    If("if", PK::Literal, TC::Keyword, TP::High),
-    Else("else", PK::Literal, TC::Keyword, TP::High),
-    Loop("loop", PK::Literal, TC::Keyword, TP::High),
-    For("for", PK::Literal, TC::Keyword, TP::High),
-    In("in", PK::Literal, TC::Keyword, TP::High),
-    While("while", PK::Literal, TC::Keyword, TP::High),
-    Public("public", PK::Literal, TC::Keyword, TP::High),
+    Use("use", PK::Literal, TC::Keyword, TP::Normal),
+    As("as", PK::Literal, TC::Keyword, TP::Normal),
+    Type("type", PK::Literal, TC::Keyword, TP::Normal),
+    Struct("struct", PK::Literal, TC::Keyword, TP::Normal),
+    Enum("enum", PK::Literal, TC::Keyword, TP::Normal),
+    Ports("ports", PK::Literal, TC::Keyword, TP::Normal),
+    Module("module", PK::Literal, TC::Keyword, TP::Normal),
+    Function("function", PK::Literal, TC::Keyword, TP::Normal),
+    Combinatorial("combinatorial", PK::Literal, TC::Keyword, TP::Normal),
+    Clocked("clocked", PK::Literal, TC::Keyword, TP::Normal),
+    Const("const", PK::Literal, TC::Keyword, TP::Normal),
+    Val("val", PK::Literal, TC::Keyword, TP::Normal),
+    Var("var", PK::Literal, TC::Keyword, TP::Normal),
+    Input("input", PK::Literal, TC::Keyword, TP::Normal),
+    Output("output", PK::Literal, TC::Keyword, TP::Normal),
+    Async("async", PK::Literal, TC::Keyword, TP::Normal),
+    Sync("sync", PK::Literal, TC::Keyword, TP::Normal),
+    Return("return", PK::Literal, TC::Keyword, TP::Normal),
+    Break("break", PK::Literal, TC::Keyword, TP::Normal),
+    Continue("continue", PK::Literal, TC::Keyword, TP::Normal),
+    True("true", PK::Literal, TC::Keyword, TP::Normal),
+    False("false", PK::Literal, TC::Keyword, TP::Normal),
+    If("if", PK::Literal, TC::Keyword, TP::Normal),
+    Else("else", PK::Literal, TC::Keyword, TP::Normal),
+    Loop("loop", PK::Literal, TC::Keyword, TP::Normal),
+    For("for", PK::Literal, TC::Keyword, TP::Normal),
+    In("in", PK::Literal, TC::Keyword, TP::Normal),
+    While("while", PK::Literal, TC::Keyword, TP::Normal),
+    Public("public", PK::Literal, TC::Keyword, TP::Normal),
 
     // misc symbols
-    Semi(";", PK::Literal, TC::Symbol, TP::Low),
-    Colon(":", PK::Literal, TC::Symbol, TP::Low),
-    Comma(",", PK::Literal, TC::Symbol, TP::Low),
-    Arrow("->", PK::Literal, TC::Symbol, TP::Low),
-    Underscore("_", PK::Literal, TC::Symbol, TP::Low),
-    ColonColon("::", PK::Literal, TC::Symbol, TP::Low),
+    Semi(";", PK::Literal, TC::Symbol, TP::Normal),
+    Colon(":", PK::Literal, TC::Symbol, TP::Normal),
+    Comma(",", PK::Literal, TC::Symbol, TP::Normal),
+    Arrow("->", PK::Literal, TC::Symbol, TP::Normal),
+    Underscore("_", PK::Literal, TC::Symbol, TP::Normal),
+    ColonColon("::", PK::Literal, TC::Symbol, TP::Normal),
 
     // braces
-    OpenC("{", PK::Literal, TC::Symbol, TP::Low),
-    CloseC("}", PK::Literal, TC::Symbol, TP::Low),
-    OpenR("(", PK::Literal, TC::Symbol, TP::Low),
-    CloseR(")", PK::Literal, TC::Symbol, TP::Low),
-    OpenS("[", PK::Literal, TC::Symbol, TP::Low),
-    CloseS("]", PK::Literal, TC::Symbol, TP::Low),
+    OpenC("{", PK::Literal, TC::Symbol, TP::Normal),
+    CloseC("}", PK::Literal, TC::Symbol, TP::Normal),
+    OpenR("(", PK::Literal, TC::Symbol, TP::Normal),
+    CloseR(")", PK::Literal, TC::Symbol, TP::Normal),
+    OpenS("[", PK::Literal, TC::Symbol, TP::Normal),
+    CloseS("]", PK::Literal, TC::Symbol, TP::Normal),
 
     // operators
-    Dot(".", PK::Literal, TC::Symbol, TP::Low),
-    Dots("..", PK::Literal, TC::Symbol, TP::Low),
-    DotsEq("..=", PK::Literal, TC::Symbol, TP::Low),
-    AmperAmper("&&", PK::Literal, TC::Symbol, TP::Low),
-    PipePipe("||", PK::Literal, TC::Symbol, TP::Low),
-    EqEq("==", PK::Literal, TC::Symbol, TP::Low),
-    Neq("!=", PK::Literal, TC::Symbol, TP::Low),
-    Gte(">=", PK::Literal, TC::Symbol, TP::Low),
-    Gt(">", PK::Literal, TC::Symbol, TP::Low),
-    Lte("<=", PK::Literal, TC::Symbol, TP::Low),
-    Lt("<", PK::Literal, TC::Symbol, TP::Low),
-    Amper("&", PK::Literal, TC::Symbol, TP::Low),
-    Circumflex("^", PK::Literal, TC::Symbol, TP::Low),
-    Pipe("|", PK::Literal, TC::Symbol, TP::Low),
-    LtLt("<<", PK::Literal, TC::Symbol, TP::Low),
-    GtGt(">>", PK::Literal, TC::Symbol, TP::Low),
-    Plus("+", PK::Literal, TC::Symbol, TP::Low),
-    Minus("-", PK::Literal, TC::Symbol, TP::Low),
-    Star("*", PK::Literal, TC::Symbol, TP::Low),
-    Slash("/", PK::Literal, TC::Symbol, TP::Low),
-    Percent("%", PK::Literal, TC::Symbol, TP::Low),
-    Bang("!", PK::Literal, TC::Symbol, TP::Low),
-    StarStar("**", PK::Literal, TC::Symbol, TP::Low),
+    Dot(".", PK::Literal, TC::Symbol, TP::Normal),
+    Dots("..", PK::Literal, TC::Symbol, TP::Normal),
+    DotsEq("..=", PK::Literal, TC::Symbol, TP::Normal),
+    AmperAmper("&&", PK::Literal, TC::Symbol, TP::Normal),
+    PipePipe("||", PK::Literal, TC::Symbol, TP::Normal),
+    EqEq("==", PK::Literal, TC::Symbol, TP::Normal),
+    Neq("!=", PK::Literal, TC::Symbol, TP::Normal),
+    Gte(">=", PK::Literal, TC::Symbol, TP::Normal),
+    Gt(">", PK::Literal, TC::Symbol, TP::Normal),
+    Lte("<=", PK::Literal, TC::Symbol, TP::Normal),
+    Lt("<", PK::Literal, TC::Symbol, TP::Normal),
+    Amper("&", PK::Literal, TC::Symbol, TP::Normal),
+    Circumflex("^", PK::Literal, TC::Symbol, TP::Normal),
+    Pipe("|", PK::Literal, TC::Symbol, TP::Normal),
+    LtLt("<<", PK::Literal, TC::Symbol, TP::Normal),
+    GtGt(">>", PK::Literal, TC::Symbol, TP::Normal),
+    Plus("+", PK::Literal, TC::Symbol, TP::Normal),
+    Minus("-", PK::Literal, TC::Symbol, TP::Normal),
+    Star("*", PK::Literal, TC::Symbol, TP::Normal),
+    Slash("/", PK::Literal, TC::Symbol, TP::Normal),
+    Percent("%", PK::Literal, TC::Symbol, TP::Normal),
+    Bang("!", PK::Literal, TC::Symbol, TP::Normal),
+    StarStar("**", PK::Literal, TC::Symbol, TP::Normal),
 
     // assignment operators
-    Eq("=", PK::Literal, TC::Symbol, TP::Low),
-    PlusEq("+=", PK::Literal, TC::Symbol, TP::Low),
-    MinusEq("-=", PK::Literal, TC::Symbol, TP::Low),
-    StarEq("*=", PK::Literal, TC::Symbol, TP::Low),
-    SlashEq("/=", PK::Literal, TC::Symbol, TP::Low),
-    PercentEq("%=", PK::Literal, TC::Symbol, TP::Low),
-    AmperEq("&=", PK::Literal, TC::Symbol, TP::Low),
-    CircumflexEq("^=", PK::Literal, TC::Symbol, TP::Low),
-    BarEq("|=", PK::Literal, TC::Symbol, TP::Low),
+    Eq("=", PK::Literal, TC::Symbol, TP::Normal),
+    PlusEq("+=", PK::Literal, TC::Symbol, TP::Normal),
+    MinusEq("-=", PK::Literal, TC::Symbol, TP::Normal),
+    StarEq("*=", PK::Literal, TC::Symbol, TP::Normal),
+    SlashEq("/=", PK::Literal, TC::Symbol, TP::Normal),
+    PercentEq("%=", PK::Literal, TC::Symbol, TP::Normal),
+    AmperEq("&=", PK::Literal, TC::Symbol, TP::Normal),
+    CircumflexEq("^=", PK::Literal, TC::Symbol, TP::Normal),
+    BarEq("|=", PK::Literal, TC::Symbol, TP::Normal),
 }
 
 #[cfg(test)]
