811e6023acbaae7a0e8f8ee8db98ff7dba2ecbe3
Karel Peeters <karel.peeters.leuven@gmail.com>
2025-05-09 23:46:22 +0200

    Delete more leftover LSP client stuff and reformat.

--- lsp_client/client/src/extension.ts
+++ lsp_client/client/src/extension.ts
@@ -1,61 +1,41 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
 import {ExtensionContext} from 'vscode';
-
 import {LanguageClient, LanguageClientOptions, ServerOptions} from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	// const serverModule = context.asAbsolutePath(
-	// 	path.join('server', 'out', 'server.js')
-	// );
+    // TODO get this from a proper location
+    const binary_path = "/home/karel/Documents/hwlang/rust/target/debug/hwl_lsp_server";
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
+    const serverOptions: ServerOptions = {
+        run: {command: binary_path},
+        debug: {command: binary_path},
+    };
 
-	// TODO get this from a proper location
-	const binary_path = "/home/karel/Documents/hwlang/rust/target/debug/hwl_lsp_server";
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [
+            {scheme: 'file', language: 'hwlang'},
+            // { scheme: 'file', pattern: "**/*.kh" },
+        ],
+        // file watching is registered by the server, no need to repeat it in the client plugin
+        synchronize: {}
+    };
 
-	const serverOptions: ServerOptions = {
-		// run: { module: serverModule, transport: TransportKind.ipc },
-		// debug: {
-		// 	module: serverModule,
-		// 	transport: TransportKind.ipc,
-		// }
-		run: { command: binary_path },
-		debug: { command: binary_path },
-	};
+    // Create the language client and start the client.
+    client = new LanguageClient(
+        'languageServerExample',
+        'Language Server Example',
+        serverOptions,
+        clientOptions
+    );
 
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: [
-			{scheme: 'file', language: 'hwlang'},
-			// { scheme: 'file', pattern: "**/*.kh" },
-		],
-		// file watching is registered by the server, no need to repeat it in the client plugin
-		synchronize: {}
-	};
-
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
-
-	// Start the client. This will also launch the server
-	client.start();
+    // Start the client. This will also launch the server
+    client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
 }
