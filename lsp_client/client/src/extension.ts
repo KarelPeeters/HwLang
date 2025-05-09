0a1fd0f60ca4f169b0b6225f635faf7efc265352
Karel Peeters <karel.peeters.leuven@gmail.com>
2025-05-10 00:37:58 +0200

    Reload lsp client if lsp server binary changes.

--- lsp_client/client/src/extension.ts
+++ lsp_client/client/src/extension.ts
@@ -1,11 +1,31 @@
 import {ExtensionContext} from 'vscode';
 import {LanguageClient, LanguageClientOptions, ServerOptions} from 'vscode-languageclient/node';
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+// TODO get this from a proper location
+const binary_path = "/home/karel/Documents/hwlang/rust/target/debug/hwl_lsp_server";
 
 let client: LanguageClient;
+let watcher: fs.FSWatcher;
+
+function register_watcher(context: ExtensionContext) {
+    // Watch the parent folder instead of the binary itself so delete+move is detected too.
+    watcher = fs.watch(path.dirname(binary_path), (eventType, filename) => {
+        if (eventType === 'change' && client) {
+            client.stop().then(() => client.start());
+        }
+    });
+
+    context.subscriptions.push({
+        dispose: () => {
+            watcher.close();
+        }
+    });
+}
 
 export function activate(context: ExtensionContext) {
-    // TODO get this from a proper location
-    const binary_path = "/home/karel/Documents/hwlang/rust/target/debug/hwl_lsp_server";
+    register_watcher(context);
 
     const serverOptions: ServerOptions = {
         run: {command: binary_path},
