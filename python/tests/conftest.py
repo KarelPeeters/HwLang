4bfbb43d707c83fd6164d3f034b339eb2eb92033
Karel Peeters <karel.peeters.leuven@gmail.com>
2025-07-13 16:46:40 +0200

    Speed up maturin call in pytest.

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -3,7 +3,11 @@ import subprocess
 from pathlib import Path
 
 # Update the rust module
-subprocess.check_call(["maturin", "develop"], cwd=Path(__file__).parent / "../../rust/hwl_python/")
+subprocess.check_call(
+    # pass some args to speed up maturin, but ideally it would be fully incremental
+    ["maturin", "develop", "--compression-method", "stored", "--uv"],
+    cwd=Path(__file__).parent / "../../rust/hwl_python/"
+)
 
 # Enable rust backtraces by default for easier debugging
 if "RUST_BACKTRACE" not in os.environ:
