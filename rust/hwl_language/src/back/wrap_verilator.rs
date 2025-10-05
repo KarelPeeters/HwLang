b161249ef5c062f9c07096c25460d2cf29e8d6ec
Karel Peeters <karel.peeters.leuven@gmail.com>
2025-10-05 19:49:01 +0200

    Remove debug prints in verilator wrapper.

--- rust/hwl_language/src/back/wrap_verilator.rs
+++ rust/hwl_language/src/back/wrap_verilator.rs
@@ -179,12 +179,6 @@ impl VerilatedInstance {
             .map(|i| (buffer[i / 8] >> (i % 8)) & 1 != 0)
             .collect_vec();
 
-        let value_tried = port_info.ty.value_from_bits(&bits).ok();
-        println!(
-            "get_port '{}' {:?} {:?} -> {:?}",
-            port_info.name, port_info.ty, bits, value_tried
-        );
-
         let value = port_info
             .ty
             .value_from_bits(&bits)
@@ -216,11 +210,6 @@ impl VerilatedInstance {
             .ty
             .value_to_bits(value.inner)
             .map_err(|_: WrongType| Either::Left(VerilatorError::InternalError("to_bits failed")))?;
-
-        println!(
-            "set_port '{}' {:?} {:?} -> {:?}",
-            port_info.name, port_info.ty, value.inner, bits
-        );
 
         let size_bytes = bits.len().div_ceil(8);
         let mut buffer = vec![0u8; size_bytes];
