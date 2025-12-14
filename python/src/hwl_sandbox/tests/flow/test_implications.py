cf6f85c324c8afde1966ed8f01bf128d47d046de
Karel Peeters <karel.peeters.leuven@gmail.com>
2025-12-14 22:28:30 +0100

    Add range implication tests.

--- python/src/hwl_sandbox/tests/flow/test_implications.py
+++ python/src/hwl_sandbox/tests/flow/test_implications.py
@@ -67,11 +67,12 @@ def test_variable_checked():
 
 
 def test_implied_type_int_const():
-    def check(ty_orig: str, cond: str, ty_true: str, ty_false: str):
+    def check(cond: str, ty_true: str, ty_false: str):
+        ty_orig = "int(0..8)"
         src = f"""
         import std.types.[uint, int];
         import std.util.print;
-        module foo ports(p: in async uint(0..8)) {{
+        module foo ports(p: in async {ty_orig}) {{
             comb {{
                 const {{ print(type(p)); }}
                 if ({cond}) {{
@@ -88,14 +89,59 @@ def test_implied_type_int_const():
         c = compile_custom(src)
         with c.capture_prints() as cap:
             _ = c.resolve("top.foo")
-        assert cap.prints == [f"{ty_orig}\n", f"{ty_true}\n", f"{ty_false}\n"]
+        assert cap.prints == [f"{ty_orig}\n", f"int({ty_true})\n", f"int({ty_false})\n"]
 
-    check("int(0..8)", "p < 4", "int(0..4)", "int(4..8)")
-    check("int(0..8)", "p <= 4", "int(0..5)", "int(5..8)")
-    check("int(0..8)", "p > 4", "int(5..8)", "int(0..5)")
-    check("int(0..8)", "p >= 4", "int(4..8)", "int(0..4)")
+    check("p < 4", "0..4", "4..8")
+    check("p <= 4", "0..5", "5..8")
+    check("p > 4", "5..8", "0..5")
+    check("p >= 4", "4..8", "0..4")
 
-    check("int(0..8)", "4 > p", "int(0..4)", "int(4..8)")
-    check("int(0..8)", "4 >= p", "int(0..5)", "int(5..8)")
-    check("int(0..8)", "4 < p", "int(5..8)", "int(0..5)")
-    check("int(0..8)", "4 <= p", "int(4..8)", "int(0..4)")
+    check("4 > p", "0..4", "4..8")
+    check("4 >= p", "0..5", "5..8")
+    check("4 < p", "5..8", "0..5")
+    check("4 <= p", "4..8", "0..4")
+
+
+def test_implied_type_int_int():
+    def check(cond: str, ty_true_a: str, ty_false_a: str):
+        ty_orig_a = "int(0..8)"
+        ty_orig_b = "int(2..6)"
+        src = f"""
+        import std.types.[uint, int];
+        import std.util.print;
+        module foo ports(a: in async {ty_orig_a}, b: in async {ty_orig_b}) {{
+            comb {{
+                const {{ print(type(a)); print(type(b)); }}
+                if ({cond}) {{
+                    val va = a;
+                    val vb = b;
+                    const {{ print(type(va)); print(type(vb)); }}
+                }} else {{
+                    val va = a;
+                    val vb = b;
+                    const {{ print(type(va)); print(type(vb)); }}
+                }}
+            }}
+        }}
+        """
+
+        c = compile_custom(src)
+        with c.capture_prints() as cap:
+            _ = c.resolve("top.foo")
+        assert cap.prints == [
+            f"{ty_orig_a}\n", f"{ty_orig_b}\n",
+            f"int({ty_true_a})\n", f"{ty_orig_b}\n",
+            f"int({ty_false_a})\n", f"{ty_orig_b}\n"
+        ]
+
+    # ty_orig_a = "int(0..8)"
+    # ty_orig_b = "int(2..6)"
+    check("a < b", "0..5", "2..8")
+    check("a <= b", "0..6", "3..8")
+    check("a > b", "3..8", "0..6")
+    check("a >= b", "2..8", "0..5")
+
+    check("b > a", "0..5", "2..8")
+    check("b >= a", "0..6", "3..8")
+    check("b < a", "3..8", "0..6")
+    check("b <= a", "2..8", "0..5")
