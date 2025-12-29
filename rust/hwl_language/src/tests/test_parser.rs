50a9420c533fc2056e0788cb67a3295a8b0f894c
Karel Peeters <karel.peeters.leuven@gmail.com>
2025-12-29 16:04:09 +0100

    Fix path of std parser test.

--- rust/hwl_language/src/tests/test_parser.rs
+++ rust/hwl_language/src/tests/test_parser.rs
@@ -20,25 +20,14 @@ fn test_parse(path: impl AsRef<Path>) {
 }
 
 #[test]
-fn parse_std_types() {
-    test_parse("../../design/project/std/types.kh")
-}
-
-#[test]
-fn parse_std_util() {
-    test_parse("../../design/project/std/util.kh")
-}
-
-#[test]
-fn parse_top() {
-    test_parse("../../design/project/top.kh")
-}
-
-#[test]
 fn parse_std_all() {
-    recurse_for_each_file::<IoErrorWithPath>(Path::new("../../design/project/std"), |_, entry| {
+    let mut count = 0;
+    recurse_for_each_file::<IoErrorWithPath>(Path::new("../hwl_std/src/std"), |_, entry| {
         test_parse(entry);
+        count += 1;
         Ok(())
     })
     .unwrap();
+
+    assert!(count > 0);
 }
