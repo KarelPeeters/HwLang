73a5bb52a1ea0997c918b57e783fe80a54e1a9e0
Karel Peeters <karel.peeters.leuven@gmail.com>
2026-01-17 00:43:04 +0100

    Fully switch to UV for python dependency management.

--- python/src/hwl_sandbox/tests/conftest.py
+++ python/src/hwl_sandbox/tests/conftest.py
@@ -3,30 +3,9 @@ import subprocess
 from pathlib import Path
 
 import pytest
-import sys
 from _pytest._py.path import LocalPath
 
-# Only do these once in the root process (when using pytest-xdist)
-if "PYTEST_SKIP_RUST_BUILD" not in os.environ and "PYTEST_XDIST_WORKER" not in os.environ:
-    # Update the rust module
-    #   (be careful not to import hwl before this point)
-    assert "hwl" not in sys.modules, "The hwl module was imported before it has been recompiled"
-
-    try:
-        subprocess.check_output(
-            # pass some args to speed up maturin, but ideally it would be fully incremental
-            ["maturin", "develop", "--compression-method", "stored", "--uv"],
-            cwd=Path(__file__).parent / "../../../../rust/hwl_python/",
-            stderr=subprocess.STDOUT,
-            text=True
-        )
-    except subprocess.CalledProcessError as e:
-        print(e.output)
-        raise
-
-    # Enable rust backtraces by default for easier debugging
-    # enable_rust_backtraces()
-
+if "PYTEST_XDIST_WORKER" not in os.environ:
     # Tell verilator to use ccache if installed
     try:
         subprocess.check_output(["ccache", "--version"])
