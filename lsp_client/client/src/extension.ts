3eda2e2fbdab1a5f64a8d7899d57b96a5983338f
Karel Peeters <karel.peeters.leuven@gmail.com>
2026-03-07 20:26:17 +0100

    Make VSCode lsp server path configurable.

--- lsp_client/client/src/extension.ts
+++ lsp_client/client/src/extension.ts
@@ -1,47 +1,61 @@
-import {ExtensionContext} from 'vscode';
+import {ExtensionContext, workspace} from 'vscode';
 import {LanguageClient, LanguageClientOptions, ServerOptions} from 'vscode-languageclient/node';
 import * as fs from "node:fs";
 import * as path from "node:path";
 
-// TODO get this from a proper location
-const binary_path = "/home/karel/Documents/hwlang/rust/target/debug/hwl_lsp_server";
+let client: LanguageClient | undefined;
+let watcher: fs.FSWatcher | undefined;
 
-let client: LanguageClient;
-
-// Restart the client when the server binary changes, to emulate hot-reloading.
-let watcher: fs.FSWatcher;
-function register_watcher(context: ExtensionContext) {
-    // Watch the parent folder instead of the binary itself so delete+move is detected too.
-    watcher = fs.watch(path.dirname(binary_path), (eventType, _filename) => {
-        if (eventType === 'change' && client) {
-            client.stop().then(() => client.start());
-        }
-    });
-
-    context.subscriptions.push({
-        dispose: () => {
-            watcher.close();
-        }
-    });
+function getServerPath(): string | undefined {
+    return workspace.getConfiguration('hwlang.server').get<string>('path') || undefined;
 }
 
-export function activate(context: ExtensionContext) {
-    register_watcher(context);
+function disposeWatcher() {
+    if (watcher) {
+        watcher.close();
+        watcher = undefined;
+    }
+}
 
+async function stopClient() {
+    if (client) {
+        await client.stop();
+        client = undefined;
+    }
+}
+
+async function startClient(context: ExtensionContext) {
+    // stop the existing client if any
+    disposeWatcher();
+    await stopClient();
+
+    // figure out the server path
+    const serverPath = getServerPath();
+    if (!serverPath) {
+        // log a warning here (but don't show a popup)
+        console.warn('HwLang LSP: No server path configured, LSP client not started');
+        return;
+    }
+
+    // register watcher to handle the server binary updating
+    //   watch the parent folder instead of the binary itself so delete+move is detected too.
+    watcher = fs.watch(path.dirname(serverPath), (eventType, _filename) => {
+        if (eventType === 'change' && client) {
+            client.stop().then(() => client?.start());
+        }
+    });
+    context.subscriptions.push({dispose: disposeWatcher});
+
+    // start the client
     const serverOptions: ServerOptions = {
-        run: {command: binary_path},
-        debug: {command: binary_path},
+        run: {command: serverPath},
+        debug: {command: serverPath},
     };
-
     const clientOptions: LanguageClientOptions = {
         documentSelector: [
             {scheme: 'file', language: 'hwlang'},
-            // { scheme: 'file', pattern: "**/*.kh" },
         ],
-        // file watching is registered by the server, no need to repeat it in the client plugin
-        synchronize: {}
     };
-
     client = new LanguageClient(
         'hwl-lsp',
         'HwLang LSP',
@@ -51,7 +65,24 @@ export function activate(context: ExtensionContext) {
     client.start();
 }
 
+// noinspection JSUnusedGlobalSymbols
+export function activate(context: ExtensionContext) {
+    // restart the client when the configured path changes
+    context.subscriptions.push(
+        workspace.onDidChangeConfiguration(e => {
+            if (e.affectsConfiguration('hwlang.server.path')) {
+                startClient(context);
+            }
+        })
+    );
+
+    // start the initial client
+    startClient(context);
+}
+
+// noinspection JSUnusedGlobalSymbols
 export function deactivate(): Thenable<void> | undefined {
+    disposeWatcher();
     if (!client) {
         return undefined;
     }
