56cbd8cf61a234de5470bdbd9fd1f03af8c8ad4a
Karel Peeters <karel.peeters.leuven@gmail.com>
2026-04-06 14:37:14 +0200

    External test setup

--- rust/hwl_vhdl/src/tests/mod.rs
+++ rust/hwl_vhdl/src/tests/mod.rs
@@ -1,10 +1,13 @@
 use crate::syntax::parser::{parse_error_to_diagnostic, parse_file_content_without_recovery};
 use hwl_common::diagnostic::{diags_to_string, Diagnostics};
 use hwl_common::source::SourceDatabase;
+use std::path::Path;
 
 // TODO test standard packages
 // TODO copy over all LRM examples
 // TODO use test cases from nvc and ghdl
+pub mod test_cases_ghdl;
+pub mod test_cases_nvc;
 pub mod test_syntax_custom;
 pub mod test_syntax_lrm;
 
@@ -25,5 +28,37 @@ pub fn test_parse(src: &str) {
             println!("{}", diags_to_string(&source, diags.finish(), true));
             panic!();
         }
+    }
+}
+
+pub fn test_parse_files(paths: &[&str]) {
+    let mut source = SourceDatabase::new();
+    let diags = Diagnostics::new();
+
+    for &path_rel in paths {
+        let path = Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join(path_rel);
+        println!("{:?}", path);
+
+        let src = std::fs::read_to_string(&path).unwrap();
+
+        let file = source.add_file(path_rel.to_owned(), src.to_owned());
+        let result = parse_file_content_without_recovery(file, &src);
+
+        match result {
+            Ok(result) => {
+                println!("{:#?}", result);
+            }
+            Err(e) => {
+                let _ = parse_error_to_diagnostic(e).report(&diags);
+            }
+        }
+    }
+
+    let diags = diags.finish();
+    let any_diag = !diags.is_empty();
+    println!("{}", diags_to_string(&source, diags, true));
+
+    if any_diag {
+        panic!("failed");
     }
 }
